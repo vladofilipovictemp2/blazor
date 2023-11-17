f8d74a520a8577720880a39d4b4fc097789df2cb
Vladimir Filipovic <vladofilipovic@hotmail.com>
2023-11-17 01:30:31 +0100

    blazor

---
+++ 01a-simple/Program.cs
@@ -0,0 +1,13 @@
+using BlazorBootstrap;
+using _01a_simple_bootstrap;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorBootstrap();
+
+await builder.Build().RunAsync();
